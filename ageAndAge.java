72153feb75db28f35b7c5d68b7cf22967b202817
fatehhundal <fshundal@hotmail.com>
2026-06-27 12:11:14 +0800

    ageAndAge

---
+++ ageAndAge.java
@@ -0,0 +1,11 @@
+public class ageAndAge {
+    int age;
+    ageAndAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        ageAndAge obj = new ageAndAge(24);
+        System.out.println("obj.age = " + obj.age);
+    }
+}
