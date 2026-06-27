2eb6af9b4ae17d424b67b6609145feefe4685301
fatehhundal <fshundal@hotmail.com>
2026-06-27 11:59:22 +0800

    parentAndChild.java

---
+++ parentAndChild.java
@@ -0,0 +1,21 @@
+class Parent {
+    int age,id;
+    String name;
+    void naming(String name) {
+        System.out.println("Name: " + name);
+    }
+}
+
+class Child extends Parent {
+    void ageN(int age) {
+        System.out.println("Age of student: " + age);
+    }
+}
+
+class parentAndChild {
+    public static void main(String [] er) {
+        Child s = new Child();
+        s.naming("Aashish");
+        s.ageN(14);
+    }
+}
